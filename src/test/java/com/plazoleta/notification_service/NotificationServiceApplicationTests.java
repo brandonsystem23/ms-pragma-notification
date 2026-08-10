ab490d665652a951876af3c5e4143ee7ade190d1
Brandon Briones Rojas <brandon20kaido@gmail.com>
2026-08-10 17:10:16 -0500

    enviar pin de seguridad

---
+++ src/test/java/com/plazoleta/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plazoleta.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
